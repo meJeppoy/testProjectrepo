cb726473f3ba1093bedcb55d5ecab2d35179d2d1
Jeff <jcustodiox@gmail.com>
2023-09-09 15:30:51 +0800

    code updates to setup kaggle

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from utils import pandas as pd
+from utils import numpy as np
+from utils import kaggle
+
+
 if __name__ == "__main__":
     pass
--- utils.py
+++ utils.py
@@ -1,2 +1,3 @@
 import numpy
 import pandas as pd
+import kaggle
