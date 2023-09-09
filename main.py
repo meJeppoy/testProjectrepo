fce71bab015700201767a183e9528d2ad5387205
Jeff <jcustodiox@gmail.com>
2023-09-10 02:34:11 +0800

    added unzip

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-# from utils import pandas as pd
-# from utils import numpy as np
-from utils import streamlit
+import pandas as pd
+import numpy as np
+import streamlit
 import kaggle
+import zipfile
 
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
 api.authenticate()
 api.dataset_download_file("arshkon/linkedin-job-postings", file_name="job_postings.csv")
+
+# Unzip the dataset downloaded from kaggle
+with zipfile.ZipFile("job_postings.csv.zip", "r") as zipref:
+    zipref.extractall()
