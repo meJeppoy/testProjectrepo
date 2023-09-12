022c26b573d6e573c35d57e356f0e7926dc0c50a
Jeff <jcustodiox@gmail.com>
2023-09-13 04:19:28 +0800

    added streamlit codes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import pandas as pd
-import numpy as np
-import streamlit
 import kaggle
 import zipfile
+import pandas as pd
+import streamlit as st
+
 
 # Downloading dataset using Kaggle API
+
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
@@ -12,5 +13,38 @@ api.authenticate()
 api.dataset_download_file("arshkon/linkedin-job-postings", file_name="job_postings.csv")
 
 # Unzip the dataset downloaded from kaggle
+
 with zipfile.ZipFile("job_postings.csv.zip", "r") as zipref:
     zipref.extractall()
+
+# Creating the streamlit web app using the kaggle dataset
+
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+model_training = st.beta_container()
+
+with header:
+    st.title("LinkedIn Jobs Analytics")
+    st.text("Interactive analysis of jobs posting data from LinkedIn")
+
+with dataset:
+    st.title("2023 LinkedIn Jobs Posting dataset")
+    st.text(
+        'The dataset was derived from kaggle as contributed by user named "arshkon"'
+    )
+
+    jobs_data = pd.read_csv("job_postings.csv")
+    st.write(jobs_data.head(10))
+
+
+with features:
+    st.header(
+        "This section showcases an depth analytics and features of the jobs posting data"
+    )
+
+with model_training:
+    st.header("This section trains our data model")
+    st.text(
+        "This section allows you to choose hyperparameters of the model and see performance changes:"
+    )
